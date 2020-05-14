3adc9165463b307e9a169eb1d74562dc0f3a1c86
Ayush Shukla <shuklaayush247@gmail.com>
2020-05-15 01:58:16 +0530

    Switch to horizontal scrolling only for mobile

--- src/constants.js
+++ src/constants.js
@@ -3,10 +3,6 @@ export const STATE_ROW_STATISTICS = [
   'active',
   'recovered',
   'deaths',
-  'confirmed',
-  'active',
-  'recovered',
-  'deaths',
 ];
 export const DISTRICT_ROW_STATISTICS = [
   'confirmed',
